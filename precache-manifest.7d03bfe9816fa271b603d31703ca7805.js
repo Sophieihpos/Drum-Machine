f64c916d6c25cda056ad7e56bc0a123ee1cafd07
Sophieihpos <sophie.emma.chapman@gmail.com>
2020-03-18 07:09:26 +1100

    Updates

---
+++ precache-manifest.7d03bfe9816fa271b603d31703ca7805.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4625d91cde92eab3328ae6e047a529d0",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "6450017847aa4b66e4b4",
+    "url": "/drum-machine/static/css/main.05053a90.chunk.css"
+  },
+  {
+    "revision": "516055fe515abdfb4ad1",
+    "url": "/drum-machine/static/js/2.4c8b0c94.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/drum-machine/static/js/2.4c8b0c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6450017847aa4b66e4b4",
+    "url": "/drum-machine/static/js/main.99d04308.chunk.js"
+  },
+  {
+    "revision": "6f9973ce3a3e927949be",
+    "url": "/drum-machine/static/js/runtime-main.2b54592e.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "/drum-machine/static/media/click1.7a7930a2.wav"
+  },
+  {
+    "revision": "fe0ded65f5f6b999883ee7aa4cf418c4",
+    "url": "/drum-machine/static/media/click2.fe0ded65.wav"
+  }
+]);
